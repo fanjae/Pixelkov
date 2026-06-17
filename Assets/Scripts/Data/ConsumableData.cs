205fb7ae0a3863269f4a4257b66ce35c5c8eb78c
FanJae <hjjang45@gmail.com>
2026-06-17 15:08:34 +0900

    feat : 소비 아이템 데이터 클래스 및 소비 아이템 타입 추가
    - 아래와 같은 기능을 추가했습니다.
    
    [ConsumableData.cs]
    - 소비 아이템 타입(회복과 회피 수치 회복 정도 분류)
    - 회피 수치 회복은 확장의 여지로 남겨 두었습니다.
    - 회복 수치

---
+++ Assets/Scripts/Data/ConsumableData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item/Consumable")]
+public class ConsumableData : ItemData
+{
+    // 소비 아이템
+    // 타입(순수 회복, 회피 수치 회복)
+    // 값
+
+    [Header("Consumable")]
+    [SerializeField] private ConsumableType consumableType;
+    [SerializeField] private int value;
+
+    public ConsumableType ConsuabmeType => consumableType;
+    public int Value => value;
+
+    public override void Use(Player player)
+    {
+        // player.UseConsumable(this);
+    }
+
+    // 소비 데이터
+    // - 소비 아이템 타입
+    // - 
+}

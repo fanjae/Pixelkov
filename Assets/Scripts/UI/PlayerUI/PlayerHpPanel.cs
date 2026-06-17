fd6c0c07ab9e12107043b9cf6d066b33f664a6f6
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-17 15:04:02 +0900

    feat : PlayerHpPanel 기능 구현
    - 플레이어의 현재 체력과 최대 체력을 받으면 해당 값을 토대로 UI를 갱신하는 기능

---
+++ Assets/Scripts/UI/PlayerUI/PlayerHpPanel.cs
@@ -0,0 +1,41 @@
+﻿using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHpPanel : MonoBehaviour
+{
+    [SerializeField] private Image fillImage;   // 체력바
+    [SerializeField] private TextMeshProUGUI hpText;    // 체력 수치 표시
+
+    private void Start()
+    {
+        // 플레이어 추가하기 이전 임시 로직
+        Init(100, 100);
+    }
+
+    /// <summary>
+    /// 게임 시작시 플레이어 HP기반으로 초기화 해주는 메서드입니다.
+    /// </summary>
+    /// <param name="currentHp"></param>
+    /// <param name="maxHp"></param>
+    public void Init(int currentHp, int maxHp)
+    {
+        float ratio = (float)currentHp / maxHp;
+        fillImage.fillAmount = ratio;
+        hpText.text = $"{currentHp} / {maxHp}";
+    }
+
+    /// <summary>
+    /// 주어지는 체력을 기반으로 HpUI를 갱신하는 메서드
+    /// </summary>
+    /// <param name="currentHp">현재 체력</param>
+    /// <param name="maxHp">최대 체력</param>
+    public void HpUIUpdate(int currentHp, int maxHp)
+    {
+        float ratio = (float)currentHp / maxHp; // 체력 비율을 구해서 DOTween을 사용해 fillAmount 조정
+        fillImage.DOKill();
+        fillImage.DOFillAmount(ratio, 0.05f).SetEase(Ease.OutQuad);
+        hpText.text = $"{currentHp} / {maxHp}";
+    }
+}

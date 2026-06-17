f3efbfc75aa005490b7890d4b061e4da5d8d96b1
FanJae <hjjang45@gmail.com>
2026-06-17 15:11:14 +0900

    feat : 방어구 아이템 데이터 클래스 추가
    - 아래와 같은 기능을 추가했습니다.
    [ArmorData.cs]
    - 방어구 아이템 데이터
    - 기본 방어력
    - 최대 강화 횟수
    - 강화당 코스트(골드)

---
+++ Assets/Scripts/Data/ArmorData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item/Armor")]
+public class ArmorData : ItemData
+{
+    [Header("Armor")]
+    [SerializeField] private int defense;
+    [SerializeField] private int maxUpgradeCount;
+    [SerializeField] private int upgradeCostPerLevel;
+
+    public int Defense => defense;
+    public int MaxUpgradeCount => maxUpgradeCount;
+    public int UpGradeCostPerLevel => upgradeCostPerLevel;
+
+    public override void Use(Player player)
+    {
+        // player.EquipArmor(this);
+    }
+
+    // 방어구 아이템 데이터
+    // - 기본 방어력
+    // - 최대 강화 횟수
+    // - 강화당 코스트
+
+}

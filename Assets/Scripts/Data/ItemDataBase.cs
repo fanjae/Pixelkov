f56d7666ab56e4b5bcbba7a8fb959e3940f394d3
FanJae <hjjang45@gmail.com>
2026-06-17 13:10:16 +0900

    Feat : 아이템 데이터 베이스 구현
    - ItemData 목록을 관리하는 ItemDatabase ScriptableObject 추가
    - ItemID 기반 조회를 위한 Dictionary 기반 검색
    - GetITem 호출 시 초기화되지 않을 때 Initialize를 하도록 처리

---
+++ Assets/Scripts/Data/ItemDataBase.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemDatabase", menuName = "Item/Item Database")]
+public class ItemDatabase : ScriptableObject
+{
+    // 전체 아이템 목록
+    [SerializeField] private List<ItemData> items;
+
+    // ItemId로 찾기 위한 Dictionary
+    private Dictionary<int, ItemData> itemMap;
+
+    // Items 리스트를 ItemId 기준 Dictionary로 변환
+    public void Initialize()
+    {
+        itemMap = new Dictionary<int, ItemData>();
+
+        foreach (ItemData item in items)
+        {
+            if (item == null) continue;
+
+            itemMap[item.ItemId] = item;
+        }
+    }
+
+    // ItemID에 해당하는 ItemData 반환
+    public ItemData GetItem(int itemId)
+    {
+        // 아직 초기화 되지 않았다면 1회 초기화 진행
+        if (itemMap == null) Initialize();
+
+        itemMap.TryGetValue(itemId, out ItemData item);
+        return item;
+    }
+}

c9183473bb9d811ae4bb10352551e39b9bbbdcd0
FanJae <hjjang45@gmail.com>
2026-06-17 15:46:29 +0900

    feat : 재료 아이템 데이터 클래스 추가
    - 아래와 같은 기능을 추가했습니다.
    [MaterialData]
    - 재료는 고유 정보가 없고 직접 사용을 하지 않으므로 ItemData 상속 처리

---
+++ Assets/Scripts/Data/MaterialData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item/MaterialData")]
+public class MaterialData : ItemData
+{
+    public override void Use(Player player)
+    {
+        // 재료도 직접 사용은 안함
+    }
+}

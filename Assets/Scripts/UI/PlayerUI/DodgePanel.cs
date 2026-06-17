55956f0d59c7bd68606968102aba8307465b0904
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-17 15:08:38 +0900

    feat : PlayerDodgePanel 구현
    - PlayerDodgePanel
    플레이어의 회피 카운트와 값을 통해 하위 패널에 적절한 수치를 전달합니다.
    
    - DodgePanel
    제공받은 수치를 기반으로 fillAmount값을 수정합니다. 만약 값이 최대라면 색상을 변경합니다.

---
+++ Assets/Scripts/UI/PlayerUI/DodgePanel.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DodgePanel : MonoBehaviour
+{
+    [SerializeField] private Image fillImage;
+    private Color defaultColor = Color.cyan;
+    private Color ActiveColor = Color.blue;
+
+    private void Awake()
+    {
+        if(fillImage != null)
+        {
+            defaultColor = fillImage.color;
+        }
+    }
+
+    /// <summary>
+    /// 플레이어의 회피 게이지를 반영해서 갱신하는 메서드
+    /// </summary>
+    public void FillUpdate(float amount)
+    {
+        //fillImage.fillAmount = amount;
+        if(amount < 1f) fillImage.color = defaultColor;
+        else fillImage.color = ActiveColor;
+        fillImage.DOFillAmount(amount, 0.05f).SetEase(Ease.OutQuad);
+    }
+}

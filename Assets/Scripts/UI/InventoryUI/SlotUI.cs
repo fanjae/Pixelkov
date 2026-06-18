ccdb78af6895da92c3190369c62f4761bb1cf864
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-18 11:27:49 +0900

    feat : GuidePanel 추가
    - 해당 커밋은 리펙토링 이전 기능만 우선 구현해둔 상태입니다.
    - 현재 단계에서 구조적인 문제(리펙토링을 통해 해결할 문제)
    1. ItemDatabase가 여러곳에 할당되어 있어서 이를 한데 묶어서 관리할 필요가 있습니다.
    2. 모든 슬롯이 GuidePanel을 알고 있어야 하는 문제(높은 결합도)
    
    - 생각중인 해결안
    1. InventoryController를 만듭니다.
    2. 해당 클래스는 ItemDatabase와 Inventory정보를 갖습니다.
    3. 갖고있는 정보를 기반으로 ItemData를 만들어 슬롯과 GuidePanel를 갱신합니다.
    4. 또한 각 슬롯에 번호를 매기고, 이벤트를 할당해 GuidePanel 갱신 타이밍을 조절합니다.

--- Assets/Scripts/UI/InventoryUI/SlotUI.cs
+++ Assets/Scripts/UI/InventoryUI/SlotUI.cs
@@ -9,41 +9,47 @@ public class SlotUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, 
     [SerializeField] private TextMeshProUGUI countText;
     [SerializeField] private Image iconImage;
     [SerializeField] private GameObject subButton;
+    [SerializeField] private GuidePanel guidePanel;
     private Vector3 originScale;
+    ItemData curData = null;
 
     private void Awake()
     {
         originScale = transform.localScale;
     }
     
-    //
-    public void PaintSlot(Sprite icon, int count)
+    public void GetItemInfo(ItemData data, int count)
     {
-        iconImage.sprite = icon;
+        curData = data;
+        iconImage.sprite = curData.Icon;
         if (count <= 1) countText.text = "";
         else countText.text = count.ToString();
     }
 
-    #region 마우스 이벤트
     // 슬롯에 마우스 올리면 확대/복구
     public void OnPointerEnter(PointerEventData eventData)
     {
         transform.DOKill();
         transform.DOScale(originScale * 1.1f, 0.15f).SetEase(Ease.OutQuad);
+        if(curData != null)
+        {
+            guidePanel.gameObject.SetActive(true);
+            guidePanel.PaintGuide(curData.ItemId);
+        }
     }
     public void OnPointerExit(PointerEventData eventData)
     {
         transform.DOKill();
         transform.DOScale(originScale, 0.15f).SetEase(Ease.OutQuad);
+        guidePanel.gameObject.SetActive(false);
     }
     public void OnPointerClick(PointerEventData eventData)
     {
         // 우클릭 시 서브 버튼 활성화/비활성화
-        if(eventData.button == PointerEventData.InputButton.Right)
+        if (eventData.button == PointerEventData.InputButton.Right)
         {
             if (subButton == null) return;
             subButton.SetActive(!subButton.activeSelf);
         }
     }
-    #endregion
 }

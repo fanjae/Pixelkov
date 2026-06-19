d7cf0b1a96687ffb1841fdf7964915d3d504955f
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-19 15:10:18 +0900

    refactor : GuidePanel 불필요한 로직 수정

--- Assets/Scripts/UI/InventoryUI/GuidePanel.cs
+++ Assets/Scripts/UI/InventoryUI/GuidePanel.cs
@@ -4,7 +4,6 @@ using UnityEngine.UI;
 
 public class GuidePanel : MonoBehaviour
 {
-    [SerializeField] private ItemDatabase database;
     [SerializeField] private Image icon;
     [SerializeField] private TextMeshProUGUI itemName;
     [SerializeField] private TextMeshProUGUI description;
@@ -12,12 +11,11 @@ public class GuidePanel : MonoBehaviour
     public void PaintGuide(int itemId)
     {
         // 할당되지 않은 필드가 존재하면 return
-        if (database == null) return;
         if (icon == null) return;
         if (itemName == null) return;
         if (description == null) return;
 
-        ItemData data = database.GetItem(itemId);
+        ItemData data = InventoryUIController.Database.GetItem(itemId);
 
         if (data == null) return;
 

815d92869886f4b2d521c34379f046f5a27826d9
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-19 13:38:49 +0900

    feat : Equipment UI 추가
    ### EquipmentPanel
    - 인벤토리 화면에서 상단 3개의 슬롯이 들어있는 패널을 관리하는 기능을 합니다.
    - Equipment 정보를 갖고 해당 정보를 슬롯에 보내주어 UI를 갱신합니다.
    
    ### EquipSlotUI
    - 가져온 데이터를 통해 UI를 재설정 합니다.
    - 각 슬롯마다 Inspector창에서 Type을 설정했습니다.
    - 장비 해제 기능을 추가 예정입니다.

---
+++ Assets/Scripts/UI/InventoryUI/EquipSlotUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EquipSlotUI : MonoBehaviour
+{
+    [SerializeField] private EquipmentSlotType slotType;
+    [SerializeField] private Image iconImage;
+    public EquipmentSlotType SlotType => slotType;
+    public ItemData CurItem { get; private set; }
+    
+    /// <summary>
+    /// 슬롯의 데이터를 가져와 UI에 적용하는 메서드
+    /// </summary>
+    public void SetSlotInfo(EquipmentSlot slot)
+    {
+        ItemDatabase database = InventoryUIController.Database;
+        if (database == null) return;
+
+        int getId = slot.ItemId;
+
+        ItemData itemInfo = database.GetItem(getId);
+        if (itemInfo == null) return;
+
+        CurItem = itemInfo;
+        iconImage.sprite = CurItem.Icon;
+    }
+}
---
+++ Assets/Scripts/UI/InventoryUI/EquipmentPanel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public class EquipmentPanel : MonoBehaviour
+{
+    [Header("아이템 슬롯")]
+    [SerializeField] private EquipSlotUI[] equipSlotUIs;  // 장비창에 있는 슬롯 오브젝트들
+    private Equipment equipment;                // 장비창의 데이터를 받을 변수
+
+    /// <summary>
+    /// 장비 정보를 제공받는 메서드.
+    /// </summary>
+    public void AllocateEquipment(Equipment newEquipment)
+    {
+        equipment = newEquipment;
+    }
+
+    /// <summary>
+    /// 모든 슬롯을 다시 그리는 메서드
+    /// </summary>
+    public void PaintEquipmentAll()
+    {
+        foreach(var slot in equipment.Slots)
+        {
+            PaintSlotUI(slot.Key, slot.Value);
+        }
+    }
+
+    private void PaintSlotUI(EquipmentSlotType type, EquipmentSlot slot)
+    {
+        // 장비 정보를 받지 못한 경우
+        if (equipment == null) return;
+
+        foreach(EquipSlotUI equipSlot in equipSlotUIs)
+        {
+            if (equipSlot == null) return;
+
+            if(equipSlot.SlotType == type)
+            {
+                equipSlot.SetSlotInfo(slot);
+                break;
+            }
+        }
+    }
+}

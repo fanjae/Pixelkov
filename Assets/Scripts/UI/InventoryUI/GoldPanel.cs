d835caa12bdccac579d8e492a4c4734be7234a0a
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-22 14:18:55 +0900

    feat : GoldPanel 로직 추가
    - PlayerGoldController에 갱신 이벤트 할당/취소

--- Assets/Scripts/UI/InventoryUI/GoldPanel.cs
+++ Assets/Scripts/UI/InventoryUI/GoldPanel.cs
@@ -3,21 +3,29 @@ using UnityEngine;
 
 public class GoldPanel : MonoBehaviour
 {
-    //[SerializeField] private Player player;
+    [SerializeField] private PlayerGoldController goldController;
     [SerializeField] private TextMeshProUGUI goldText;
 
     private void OnEnable()
     {
         // 활성화 시 골드 갱신
-        GoldTextUpdate();
         // Player 골드 획득 로직 추가 시 이벤트 할당 예정
+        if(goldController != null)
+        {
+            GoldTextUpdate(goldController.Gold);
+            goldController.OnGoldChanged += GoldTextUpdate;
+        }
     }
     private void OnDisable()
     {
-        
+        if(goldController != null)
+        {
+            goldController.OnGoldChanged -= GoldTextUpdate;
+        }
     }
-    public void GoldTextUpdate()
+    public void GoldTextUpdate(int amount)
     {
         // 플레이어의 골드를 받아서 텍스트 추가
+        goldText.text = amount.ToString();
     }
 }

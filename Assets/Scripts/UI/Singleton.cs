4a8c3704ab828f2e789fa194db1fb3decc492793
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-24 11:01:42 +0900

    feat : 제네릭 싱글톤 추가

---
+++ Assets/Scripts/UI/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static T instance = null;
+    public static T Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindFirstObjectByType<T>();
+
+                if(instance == null)
+                {
+                    GameObject inst = new GameObject(typeof(T).Name);
+                    instance = inst.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}

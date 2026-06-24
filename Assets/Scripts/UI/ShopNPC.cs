6a1f1808c53db37888e14b66511d313796eebd84
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-24 16:38:24 +0900

    feat : ShopNPC 기능 구현

---
+++ Assets/Scripts/UI/ShopNPC.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ShopNPC : MonoBehaviour
+{
+    [SerializeField] private GameObject Shop;
+    public void SwitchShop()
+    {
+        Shop?.SetActive(!Shop.activeSelf);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            InputUIController.ShopAction += SwitchShop;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            InputUIController.ShopAction -= SwitchShop;
+        }
+    }
+    private void OnDestroy()
+    {
+        InputUIController.ShopAction -= SwitchShop;
+    }
+}

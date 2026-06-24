f6956faa5e5248ffc41f428b61f953521e2f48e7
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-24 11:24:38 +0900

    feat : SceneLoadManager 추가

---
+++ Assets/Scripts/UI/SceneLoadManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum SceneType
+{
+    Title, Map
+}
+public class SceneLoadManager : Singleton<SceneLoadManager>
+{
+    // 씬 이름을 타입으로 지정해서 오류 방지
+    private Dictionary<SceneType, string> SceneNames = new Dictionary<SceneType, string>()
+    {
+        {SceneType.Title, "TitleScene"},
+        {SceneType.Map, "MapScene"}
+    };
+
+    /// <summary>
+    /// SceneType에 맞춰 씬을 로드하는 메서드
+    /// </summary>
+    public void LoadScene(SceneType type)
+    {
+        SceneManager.LoadScene(SceneNames[type]);
+    }
+}

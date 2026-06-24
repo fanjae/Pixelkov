a90c7c19fc87035411c2e9f890e628132335978a
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-24 16:00:14 +0900

    feat : UI Input 기능 구현

---
+++ Assets/Scripts/UI/InputUIController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputUIController : MonoBehaviour
+{
+    public static Action ShopAction;
+
+    [SerializeField] private GameObject inventoryUI;
+    [SerializeField] private GameObject menuUI;
+
+    private InputAction InputInventory;
+    private InputAction InputMenu;
+    private InputAction InputShop;
+
+    private void Awake()
+    {
+        InputInventory = InputSystem.actions.FindAction("Inventory");
+        InputMenu = InputSystem.actions.FindAction("Menu");
+        InputShop = InputSystem.actions.FindAction("Shop"); // 기본으로 존재하는 Interact Action 재활용
+    }
+
+    void Update()
+    {
+        if(InputInventory.WasPerformedThisFrame())
+        {
+            inventoryUI?.SetActive(!inventoryUI.activeSelf);
+        }
+        if(InputMenu.WasPerformedThisFrame())
+        {
+            menuUI?.SetActive(!menuUI.activeSelf);
+        }
+        if(InputShop.WasCompletedThisFrame())
+        {
+            ShopAction?.Invoke();
+        }
+    }
+}

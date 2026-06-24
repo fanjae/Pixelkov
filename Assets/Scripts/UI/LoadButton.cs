b2e500057550ba2b7563a8ac2fa876cad48d1c8e
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-24 11:28:45 +0900

    feat : LoadButton 추가

---
+++ Assets/Scripts/UI/LoadButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class LoadButton : MonoBehaviour
+{
+    [SerializeField] private SceneType sceneType;
+    public void ClickLoadButton()
+    {
+        SceneLoadManager.Instance.LoadScene(sceneType);
+    }
+}

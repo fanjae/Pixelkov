758c78211d3dcea47eaa016b5e1d03945a982159
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-24 20:46:01 +0900

    refactor : Singleton 추상 클래스로 수정

--- Assets/Scripts/UI/Singleton.cs
+++ Assets/Scripts/UI/Singleton.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
 {
     private static T instance = null;
     private static bool isQuit = false;

142c69d28575c62c38018afd295e3f43a9c7004a
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-25 21:27:31 +0900

    feat : audio manager 기능 구현

---
+++ Assets/Scripts/UI/OptionUI/AudioManager.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    [Header("Audio Fields")]
+    [SerializeField] private AudioSource BGM_Player;
+    [SerializeField] private AudioSource SFX_Player;
+    [SerializeField] private AudioDataBase database;
+    [SerializeField] private AudioMixer audioMixer;
+
+    private void Start()
+    {
+        if(SceneLoadManager.Instance != null)
+            SceneLoadManager.Instance.OnSceneChanged += ChangeBGM;
+    }
+    private void OnDestroy()
+    {
+        if(SceneLoadManager.Instance != null)
+            SceneLoadManager.Instance.OnSceneChanged -= ChangeBGM;
+    }
+
+    /// <summary>
+    /// sfx타입을 받아서 재생 해주는 메서드
+    /// </summary>
+    public void PlaySFX(SFXType sfxType)
+    {
+        if (SFX_Player == null || database == null) return;
+
+        AudioData data = database.GetAudioData(sfxType);
+        if (data == null) return;
+
+        AudioClip nextClip = data.Clip;
+        if(nextClip == null) return;
+
+        SFX_Player.PlayOneShot(nextClip);
+    }
+
+    /// <summary>
+    /// bgm타입을 받아서 재생 해주는 메서드
+    /// </summary>
+    public void PlayBGM(BGMType bgmType)
+    {
+        if (BGM_Player == null || database == null) return;
+
+        AudioData data = database.GetAudioData(bgmType);
+        if (data == null) return;
+        
+        AudioClip nextClip = data.Clip;
+        if (nextClip == null) return;
+
+        BGM_Player.clip = nextClip;
+        BGM_Player.Play();
+    }
+
+    public void SetMasterVolume(float amount)
+    {
+        if (audioMixer == null) return;
+        audioMixer.SetFloat("Master", Mathf.Log10(amount) * 20.0f);
+    }
+    public void SetBGMVolume(float amount)
+    {
+        if (audioMixer == null) return;
+        audioMixer.SetFloat("BGM", Mathf.Log10(amount) * 20.0f);
+    }
+    public void SetSFXVolume(float amount)
+    {
+        if (audioMixer == null) return;
+        audioMixer.SetFloat("SFX", Mathf.Log10(amount) * 20.0f);
+    }
+
+    private void ChangeBGM(SceneType sceneType)
+    {
+        if (BGM_Player == null || database == null) return;
+
+        // 씬 타입에 맞는 배경음 변경
+        BGMType bgmType = ConvertToBGMType(sceneType);
+        if(bgmType == BGMType.None) return;
+
+        PlayBGM(bgmType);
+    }
+
+    private BGMType ConvertToBGMType(SceneType sceneType)
+    {
+        switch(sceneType)
+        {
+            case SceneType.Title:
+                return BGMType.Title;
+            case SceneType.Main:
+                return BGMType.Main;
+            default:
+                return BGMType.None;
+        }
+    }
+}

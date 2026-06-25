12d8bca5c11bef2b14f673f8de70d9fb950154dd
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-25 19:30:28 +0900

    feat : AudioData 추가

---
+++ Assets/Scripts/Data/AudioData.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public enum AudioType
+{
+    BGM, SFX
+}
+public enum BGMType
+{
+    None, Title, Main
+}
+public enum SFXType
+{ 
+    None, 
+}
+
+[CreateAssetMenu(fileName = "AudioData", menuName = "Audio/Audio Data")]
+public class AudioData : ScriptableObject
+{
+    [Header("Audio Clip")]
+    [SerializeField] private AudioClip clip;
+
+    [Header("Audio Clip Type")]
+    [SerializeField] private AudioType audioType;
+    [SerializeField] private BGMType bgmType;
+    [SerializeField] private SFXType sfxType;
+
+    public AudioClip Clip => clip;
+    public AudioType AudioType => audioType;
+    public BGMType BgmType => bgmType;
+    public SFXType SFXType => sfxType;
+}

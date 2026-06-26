f7546475b995f6fdd7e6bb9341c615d6e392bed9
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-26 10:43:16 +0900

    feat : 볼륨 조절 기능 추가

---
+++ Assets/Scripts/UI/OptionUI/VolumnPanel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumnPanel : MonoBehaviour
+{
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    private void Start()
+    {
+        if(AudioManager.Instance != null)
+        {
+            if (masterSlider != null)
+                masterSlider.onValueChanged.AddListener(AudioManager.Instance.SetMasterVolume);
+            if (bgmSlider != null)
+                bgmSlider.onValueChanged.AddListener(AudioManager.Instance.SetBGMVolume);
+            if (sfxSlider != null)
+                sfxSlider.onValueChanged.AddListener(AudioManager.Instance.SetSFXVolume);
+        }
+    }
+}

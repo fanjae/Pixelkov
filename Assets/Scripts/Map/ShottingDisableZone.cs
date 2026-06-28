07ab12636ed6ee05b38f095b26a31cf8fdccd418
FanJae <hjjang45@gmail.com>
2026-06-28 16:20:17 +0900

    feat : 지하 거점에서 플레이어의 사격이 이뤄지지 않도록 처리
    - 지하 거점에서는 플레이어의 사격이 불가능하도록 변경했습니다.

---
+++ Assets/Scripts/Map/ShottingDisableZone.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ShootingDisableZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player")) return;
+
+        PlayerShooter shooter = collision.GetComponent<PlayerShooter>(); // 플레이어 슛팅 관련 컴포넌트를 찾아서 블록 처리
+        if (shooter != null)
+        {
+            shooter.SetShootingBlocked(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player")) return;
+
+        PlayerShooter shooter = collision.GetComponent<PlayerShooter>(); // 플레이어 슛팅 관련 컴포넌트를 찾아서 활성화 처리
+        if (shooter != null)
+        {
+            shooter.SetShootingBlocked(false);
+        }
+    }
+}

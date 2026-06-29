fcc3a256fe9f4ea17d3e0192a428bdbed8c840f1
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-29 18:31:51 +0900

    feat : NPC 상호작용 UI 추가

---
+++ Assets/Scripts/UI/InteractCanvas.cs
@@ -0,0 +1,30 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class InteractCanvas : MonoBehaviour
+{
+    private Vector3 startPos;
+    private Vector3 targetPos;
+    [SerializeField] private Vector3 offset;
+
+    private Sequence sequence;
+
+    private void Awake()
+    {
+        startPos = transform.position;
+        targetPos = transform.position + offset;
+    }
+    private void OnEnable()
+    {
+        //DOTween.Kill(transform);
+        transform.position = startPos;
+        sequence = DOTween.Sequence();
+        sequence.Append(transform.DOMove(targetPos, 0.5f)).Append(transform.DOMove(startPos, 1f));
+        sequence.SetLoops(-1);
+    }
+    private void OnDisable()
+    {
+        if (sequence != null)
+            sequence.Kill();
+    }
+}

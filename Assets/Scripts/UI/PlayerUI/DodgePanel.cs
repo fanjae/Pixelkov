02530606c47b308007dd7f27c2384883d0b9ccd0
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-06-29 15:21:01 +0900

    feat : HUD 디테일 추가

--- Assets/Scripts/UI/PlayerUI/DodgePanel.cs
+++ Assets/Scripts/UI/PlayerUI/DodgePanel.cs
@@ -5,24 +5,13 @@ using UnityEngine.UI;
 public class DodgePanel : MonoBehaviour
 {
     [SerializeField] private Image fillImage;
-    private Color defaultColor = Color.cyan;
-    private Color ActiveColor = Color.blue;
-
-    private void Awake()
-    {
-        if(fillImage != null)
-        {
-            defaultColor = fillImage.color;
-        }
-    }
 
     /// <summary>
     /// 플레이어의 회피 게이지를 반영해서 갱신하는 메서드
     /// </summary>
     public void FillUpdate(float amount)
     {
-        fillImage.fillAmount = amount;
-        if(amount < 0.5f) fillImage.color = defaultColor;
-        else fillImage.color = ActiveColor;
+        //fillImage.fillAmount = amount;
+        fillImage.DOFillAmount(amount, 0.1f).SetEase(Ease.OutQuad);
     }
 }

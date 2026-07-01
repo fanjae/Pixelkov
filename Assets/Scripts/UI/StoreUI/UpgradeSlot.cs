43bec366d1896d9890f92bd4baa9b729ab0ce2d2
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-07-01 14:16:05 +0900

    feat : 강화창 더블클릭시 해제 추가

---
+++ Assets/Scripts/UI/StoreUI/UpgradeSlot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UpgradeSlot : MonoBehaviour, IPointerClickHandler
+{
+    public Action ReleaseEvent;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.clickCount >= 2)
+        {
+            ReleaseEvent?.Invoke();
+            eventData.clickCount = 0;
+        }
+    }
+}

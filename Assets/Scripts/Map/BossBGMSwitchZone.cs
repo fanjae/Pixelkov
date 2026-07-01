c9e9c50328f27aaa4a3abcac37f8f2a7b0a4538b
FanJae <hjjang45@gmail.com>
2026-07-02 01:06:38 +0900

    feat : BossZone BGM 추가
    - BossZone 진입시 보스 BGM이 나오기 시작하고, 나오면 보스 BGM이 종료되도록 처리

---
+++ Assets/Scripts/Map/BossBGMSwitchZone.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BGMSwitchZone : MonoBehaviour
+{
+    [SerializeField] private BGMType enterBGM;
+    [SerializeField] private BGMType exitBGM;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.Play(enterBGM);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.Play(exitBGM);
+        }
+    }
+}

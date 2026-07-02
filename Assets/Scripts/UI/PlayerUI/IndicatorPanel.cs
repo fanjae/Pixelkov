df320f7e8eaad78c38314c8a36d19861615851fe
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-07-02 13:08:55 +0900

    feat : 플레이어 HUD에 IndicatorPanel 추가

---
+++ Assets/Scripts/UI/PlayerUI/IndicatorPanel.cs
@@ -0,0 +1,28 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IndicatorPanel : MonoBehaviour
+{
+    [SerializeField] private Image outline;
+
+    private void Awake()
+    {
+        if(outline == null) outline = GetComponent<Image>();
+    }
+
+    public void HitIndicator()
+    {
+        if (outline == null) return;
+        DOTween.Kill(outline);
+        outline.color = new Color(1.0f, 0.0f, 0.0f, 0.75f);
+        outline.DOColor(new Color(1.0f, 0.0f, 0.0f, 0.0f), 0.3f);
+    }
+    public void HealIndicator()
+    {
+        if (outline == null) return;
+        DOTween.Kill(outline);
+        outline.color = new Color(0.0f, 1.0f, 0.0f, 0.75f);
+        outline.DOColor(new Color(0.0f, 1.0f, 0.0f, 0.0f), 0.3f);
+    }
+}

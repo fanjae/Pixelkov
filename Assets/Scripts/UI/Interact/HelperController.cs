f04aa469c7abae52fb80df89422420f0012bda6c
SeoN <82944267+duaehdtjs20@users.noreply.github.com>
2026-07-03 10:30:55 +0900

    feat : Helper UI 추가

---
+++ Assets/Scripts/UI/Interact/HelperController.cs
@@ -0,0 +1,36 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class HelperController : MonoBehaviour
+{
+    [SerializeField] private bool isOpen = true;
+    [SerializeField] private float ypos = 250f;
+
+    private Vector2 openPos = new Vector2(-400f, 0f);
+    private Vector2 closePos = new Vector2(0f, 0f);
+
+    private RectTransform rectTransform;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        if (isOpen) rectTransform.anchoredPosition = openPos + new Vector2(0.0f, ypos);
+        else rectTransform.anchoredPosition = closePos + new Vector2(0.0f, ypos);
+    }
+    public void SwitchHelper()
+    {
+        DOTween.Kill(rectTransform);
+        Vector2 targetPos = new Vector2(0.0f, ypos);
+        if (isOpen)
+        {
+            targetPos += closePos;
+        }
+        else
+        {
+            targetPos += openPos;
+        }
+
+        rectTransform.DOAnchorPos(targetPos, 0.3f);
+        isOpen = !isOpen;
+    }
+}
